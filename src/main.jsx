7af54fb3cbb31c178d316745b082a6b4077daaae
maneesh0001 <ajcen0006@gmail.com>
2025-08-01 21:40:40 +0545

    changes in main an dlockjason

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import AppRouter from './router/AppRouter.jsx';
 import AuthProvider from './auth/AuthProvider.jsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
+  darkMode: 'class', // Enable dark mode support
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
+
